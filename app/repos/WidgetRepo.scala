6a1185ee805eebe7bf77dc59c62f2766a742d272
Clayton Rieck <cjrieck123@gmail.com>
2016-08-08 13:52:52 -0400

    added initial project. db stuff WIP

---
+++ app/repos/WidgetRepo.scala
@@ -0,0 +1,47 @@
+package repos
+
+import javax.inject.Inject
+
+import play.api.libs.json.{JsObject, Json}
+import play.modules.reactivemongo.ReactiveMongoApi
+import play.modules.reactivemongo.json._
+import play.modules.reactivemongo.json.collection.JSONCollection
+import reactivemongo.api.ReadPreference
+import reactivemongo.api.commands.WriteResult
+import reactivemongo.bson.{BSONDocument, BSONObjectID}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait WidgetRepo {
+	def find()(implicit ec: ExecutionContext): Future[List[JsObject]]
+	def select(selector: BSONDocument)(implicit ec: ExecutionContext): Future[Option[JsObject]]
+	def update(selector: BSONDocument, update: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+	def remove(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+	def save(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult]
+}
+
+class WidgetRepoImpl @Inject() (reactiveMongoApi: ReactiveMongoApi) extends WidgetRepo {
+	def collection = reactiveMongoApi.db.collection[JSONCollection]("widgets")
+
+	override def find()(implicit ec: ExecutionContext): Future[List[JsObject]] = {
+		var queryBuilder = collection.find(Json.obj());
+		var cursor = queryBuilder.cursor[JsObject](ReadPreference.primary)
+		cursor.collect[List]()
+	}
+
+	override def select(selector: BSONDocument)(implicit ec: ExecutionContext): Future[Option[JsObject]] = {
+		collection.find(selector).one[JsObject]
+	}
+
+	override def update(selector: BSONDocument, update: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] = {
+		collection.update(selector, update)
+	}
+
+	override def remove(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] = {
+		collection.remove(document)
+	}
+
+	override def save(document: BSONDocument)(implicit ec: ExecutionContext): Future[WriteResult] = {
+		collection.update(BSONDocument("_id" -> document.get("_id").getOrElse(BSONObjectID.generate)), document, upsert = true)
+	}
+}
---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := """play-scala"""
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  jdbc,
+  cache,
+  ws,
+  "org.scalatestplus.play" %% "scalatestplus-play" % "1.5.1" % Test,
+  "org.reactivemongo" %% "play2-reactivemongo" % "0.11.14",
+  "org.reactivemongo" %% "reactivemongo-play-json" % "0.11.14"
+)
+
+resolvers += "scalaz-bintray" at "http://dl.bintray.com/scalaz/releases"
+
+
+fork in run := false
